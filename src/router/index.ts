8bc864bbe5caa8bf8e194e64646683e91ca482b5
sugerfrog <dwjhh15134790826@163.com>
2022-10-27 21:59:33 +0800

    second commit just test

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,8 +7,8 @@ Vue.use(VueRouter);
 const routes: Array<RouteConfig> = [
   {
     path: "/",
-    name: "Home",
-    component: Home,
+    name: "Login",
+    component: () => import("../views/acount/Login.vue"),
   },
   {
     path: "/about",
---
+++ src/views/acount/Login.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="login">
+    </div>
+</template>
+<script lang="ts">
+
+export default {
+    name:"login",
+}
+</script>
